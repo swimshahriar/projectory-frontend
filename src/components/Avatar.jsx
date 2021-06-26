79135e2bbc419ce4110c397ee85ca2bd983881cb
Shahriar Swim <shahriar2k18@gmail.com>
2021-06-26 23:34:01 +0600

    user profile info section

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Avatar as Image, Badge } from "@material-ui/core";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+
+// styles
+const useStyles = makeStyles((theme) => ({
+  start: {
+    marginLeft: 0,
+  },
+  center: {
+    margin: "0 auto",
+  },
+  end: {
+    marginRight: 0,
+  },
+  small: {
+    width: theme.spacing(5),
+    height: theme.spacing(5),
+  },
+  medium: {
+    width: theme.spacing(12),
+    height: theme.spacing(12),
+  },
+  large: {
+    width: theme.spacing(20),
+    height: theme.spacing(20),
+  },
+}));
+
+const Avatar = ({
+  alt,
+  src,
+  size = 12,
+  position = "center",
+  variant = "circle",
+  ...props
+}) => {
+  const classes = useStyles();
+
+  return (
+    <Image
+      alt={alt}
+      src={src}
+      variant={variant}
+      className={`${classes[size]} ${classes[position]}`}
+    />
+  );
+};
+
+export default Avatar;
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,12 +1,145 @@
 import React from "react";
-import { Container, Typography } from "@material-ui/core";
+import { Container, Typography, Box, Divider } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { useDispatch, useSelector } from "react-redux";
+import { HiLocationMarker } from "react-icons/hi";
+import { BsFillPersonFill } from "react-icons/bs";
+import { GoPrimitiveDot } from "react-icons/go";
+
+// components
+import Avatar from "../components/Avatar";
+
+// styles
+const useStyles = makeStyles((theme) => ({
+  iconCenter: {
+    display: "flex",
+    placeItems: "center",
+    gap: 5,
+  },
+  onlineBadge: {
+    border: 1.5,
+    borderStyle: "solid",
+    borderColor: theme.palette.primary.main,
+    borderRadius: 5,
+    padding: "2px 3px",
+    paddingRight: "5px",
+    opacity: 0.8,
+  },
+  mtMd: {
+    marginTop: 10,
+  },
+  mrSm: {
+    marginRight: 5,
+  },
+  textBold: {
+    fontWeight: "bold",
+  },
+}));
 
 const UserProfile = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const { uid, token } = useSelector((state) => state.auth);
+
   return (
     <Container>
-      <Typography variant="h1" align="center">
-        User Profile
-      </Typography>
+      <Box display="flex" justifyContent="center" gridGap={15} mt={5}>
+        <Box flex={40}>
+          <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            boxShadow={3}
+            borderRadius={5}
+            p={5}
+          >
+            <Box width="100%">
+              <Box display="flex" justifyContent="flex-end">
+                <Typography
+                  align="center"
+                  color="primary"
+                  className={`${classes.onlineBadge} ${classes.iconCenter}`}
+                >
+                  <GoPrimitiveDot /> Online
+                </Typography>
+              </Box>
+              <Avatar
+                alt="shahriar"
+                src="https://i.pravatar.cc/300?img=57"
+                size="large"
+                position="center"
+              />
+              <Typography
+                variant="h6"
+                component="h6"
+                align="center"
+                color="textPrimary"
+                className={classes.mtMd}
+              >
+                shahriar_swim
+              </Typography>
+              <Typography
+                variant="body1"
+                component="p"
+                align="center"
+                color="textSecondary"
+                className={classes.mtMd}
+              >
+                Experienced Software Engineer
+              </Typography>
+              <Divider className={classes.mtMd} />
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                className={classes.mtMd}
+              >
+                <Typography
+                  variant="body1"
+                  component="p"
+                  color="textSecondary"
+                  className={classes.iconCenter}
+                >
+                  <HiLocationMarker />
+                  From
+                </Typography>
+                <Typography
+                  variant="body1"
+                  component="p"
+                  color="textPrimary"
+                  className={classes.textBold}
+                >
+                  Bangladesh
+                </Typography>
+              </Box>
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                className={classes.mtMd}
+              >
+                <Typography
+                  variant="body1"
+                  component="p"
+                  color="textSecondary"
+                  className={classes.iconCenter}
+                >
+                  <BsFillPersonFill />
+                  Member Since
+                </Typography>
+                <Typography
+                  variant="body1"
+                  component="p"
+                  color="textPrimary"
+                  className={classes.textBold}
+                >
+                  {new Date().toDateString()}
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+        <Box flex={60}>Right Box</Box>
+      </Box>
     </Container>
   );
 };
