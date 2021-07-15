2c47022456df1eceacf141ecf3fb5c66f5e97fc7
Shahriar Swim <shahriar2k18@gmail.com>
2021-07-15 21:46:32 +0600

    add service form

--- src/pages/AddService.jsx
+++ src/pages/AddService.jsx
@@ -12,10 +12,14 @@ import {
   FormControl,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import { Alert } from "@material-ui/lab";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { AiFillDelete } from "react-icons/ai";
+
+// components
+import RoundedBox from "../components/RoundedBox";
 
 // styles
 const useStyles = makeStyles((theme) => ({
@@ -50,6 +54,11 @@ const useStyles = makeStyles((theme) => ({
     width: "100%",
     marginBottom: 10,
   },
+  hover: {
+    "&:hover": {
+      cursor: "pointer",
+    },
+  },
 }));
 
 // form schema
@@ -57,17 +66,14 @@ const schema = yup.object().shape({
   title: yup.string().max(50).required("required"),
   about: yup.string().required("required"),
   category: yup.string().required("required"),
-  basicName: yup.string().required("required"),
   basicPrice: yup.number().min(300).required("required"),
   basicDeliveryTime: yup.number().min(1).max(30).required("required(1 - 30)"),
-  standardName: yup.string().required("required"),
   standardPrice: yup.number().min(300).required("required"),
   standardDeliveryTime: yup
     .number()
     .min(1)
     .max(30)
     .required("required(1 - 30)"),
-  premiumName: yup.string().required("required"),
   premiumPrice: yup.number().min(300).required("required"),
   premiumDeliveryTime: yup.number().min(1).max(30).required("required(1 - 30)"),
 });
@@ -75,6 +81,16 @@ const schema = yup.object().shape({
 const AddService = () => {
   const dispatch = useDispatch();
   const classes = useStyles();
+  const [formError, setFormError] = useState(null);
+  const [category, setCategory] = useState("");
+  const [imageInputState, setImageInputState] = useState("");
+  const [images, setImages] = useState(null);
+  const [previewSource, setPreviewSource] = useState(null);
+  const [features, setFeatures] = useState({
+    basic: "",
+    standard: "",
+    premium: "",
+  });
   const [basicFeatures, setBasicFeatures] = useState(null);
   const [standardFeatures, setStandardFeatures] = useState(null);
   const [premiumFeatures, setPremiumFeatures] = useState(null);
@@ -86,8 +102,42 @@ const AddService = () => {
     reset,
   } = useForm({ resolver: yupResolver(schema) });
 
+  // preview image
+  const previewImage = (image) => {
+    if (previewSource && previewSource.length >= 3)
+      return setFormError("Max 3 images allowed!");
+    const reader = new FileReader();
+    reader.readAsDataURL(image);
+    reader.onloadend = () => {
+      setPreviewSource((prev) =>
+        prev ? [...prev, reader.result] : [reader.result]
+      );
+    };
+  };
+
+  // file input hanlder
+  const fileInputHanlder = (e) => {
+    setFormError(null);
+    const image1 = e.target.files[0];
+    previewImage(image1);
+
+    const image2 = e.target.files[1];
+    if (image2) {
+      previewImage(image2);
+    }
+
+    const image3 = e.target.files[2];
+    if (image3) {
+      previewImage(image3);
+    }
+  };
+
   // submit hanlder
-  const submitHandler = async (data) => {};
+  const submitHandler = async (data) => {
+    setFormError(null);
+    if (previewSource || previewSource.length <= 0)
+      return setFormError("Images cannot be empty!");
+  };
 
   return (
     <Container
@@ -102,10 +152,54 @@ const AddService = () => {
       </Box>
 
       <form onSubmit={handleSubmit(submitHandler)} className={classes.form}>
+        <Box mb={3}>
+          {formError && <Alert severity="error">{formError}</Alert>}
+        </Box>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          gridGap={5}
+          flexWrap="wrap"
+        >
+          {previewSource &&
+            previewSource.map((image, idx) => (
+              <img
+                key={idx}
+                src={image}
+                alt="choosen"
+                className={classes.hover}
+                style={{
+                  width: "150px",
+                  minWidth: "100px",
+                  height: "150px",
+                  minHeight: "100px",
+                }}
+                onClick={() => {
+                  setFormError(null);
+                  setPreviewSource((prev) =>
+                    prev.filter((img) => img !== image)
+                  );
+                }}
+              />
+            ))}
+        </Box>
+        <Box m={3}>
+          <Button variant="outlined" color="secondary" component="label">
+            Choose Images (max. 3)
+            <input
+              type="file"
+              multiple
+              value={imageInputState}
+              onChange={fileInputHanlder}
+              hidden
+            />
+          </Button>
+        </Box>
         <TextField
           {...register("title")}
           label="Title"
-          helperText={errors.title?.message}
+          helpertext={errors.title?.message}
           error={errors.title ? true : false}
           variant="outlined"
           className={classes.formInput}
@@ -113,7 +207,7 @@ const AddService = () => {
         <TextField
           {...register("about")}
           label="About"
-          helperText={errors.about?.message}
+          helpertext={errors.about?.message}
           error={errors.about ? true : false}
           variant="outlined"
           multiline
@@ -123,42 +217,292 @@ const AddService = () => {
         <FormControl variant="outlined" className={classes.select}>
           <InputLabel id="category">Category</InputLabel>
           <Select
-            {...register("category")}
-            helperText={errors.category?.message}
-            error={errors.category ? true : false}
             labelId="category"
             label="Category"
             variant="outlined"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
             className={classes.select}
           >
             <MenuItem value="web-developement">Web Developement</MenuItem>
             <MenuItem value="mobile-developement">Mobile Developement</MenuItem>
-            <MenuItem value="graphics-design">Graphics Design</MenuItem>
+            <MenuItem value="graphics-designing">Graphics Designing</MenuItem>
           </Select>
         </FormControl>
-        <Box width="100%">
+        {/* basic package */}
+        <Box width="100%" mt={3}>
           <Box mb={2}>
             <Typography align="center" variant="h6">
               Package - Basic
             </Typography>
           </Box>
-          <TextField
-            {...register("basicName")}
-            label="Basic Name"
-            helperText={errors.basicName?.message}
-            error={errors.basicName ? true : false}
-            variant="outlined"
-            className={classes.formInput}
-          />
+
           <TextField
             {...register("basicPrice")}
-            label="Basic Price"
-            helperText={errors.basicDeliveryTime?.message}
-            error={errors.basicDeliveryTime ? true : false}
+            label="Price"
+            helpertext={errors.basicPrice?.message}
+            error={errors.basicPrice ? true : false}
             variant="outlined"
+            type="number"
             className={classes.formInput}
           />
+          <TextField
+            {...register("basicDeliveryTime")}
+            label="Delivery Time (days)"
+            helpertext={errors.basicDeliveryTime?.message}
+            error={errors.basicDeliveryTime ? true : false}
+            variant="outlined"
+            type="number"
+            className={classes.formInput}
+          />
+          {/* features */}
+          <Box width="100%">
+            <Box display="flex" justifyContent="center" alignItems="center">
+              <TextField
+                label="Features"
+                helpertext="5 revision, 3 pages, source code, etc"
+                variant="outlined"
+                value={features.basic}
+                onChange={(e) =>
+                  setFeatures((prev) => {
+                    return { ...prev, basic: e.target.value };
+                  })
+                }
+                className={classes.formInput}
+              />
+              <Box ml={1} mt={-7}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={(e) => {
+                    if (features.basic === "") return;
+                    setBasicFeatures((prev) =>
+                      prev ? [...prev, features.basic] : [features.basic]
+                    );
+
+                    setFeatures((prev) => {
+                      return { ...prev, basic: "" };
+                    });
+                  }}
+                >
+                  Add
+                </Button>
+              </Box>
+            </Box>
+            <Box display="flex" flexWrap="wrap">
+              {!basicFeatures || basicFeatures.length <= 0 ? (
+                <Typography>Not yet added.</Typography>
+              ) : (
+                basicFeatures.map((feature, idx) => (
+                  <Box
+                    className={classes.hover}
+                    onClick={() =>
+                      setBasicFeatures((prev) =>
+                        prev.filter((item) => item !== feature)
+                      )
+                    }
+                  >
+                    <RoundedBox
+                      key={idx}
+                      light={true}
+                      icon={true}
+                      borderColor={
+                        idx % 2 == 0 ? "primary.main" : "secondary.main"
+                      }
+                    >
+                      {feature}
+                      <AiFillDelete />
+                    </RoundedBox>
+                  </Box>
+                ))
+              )}
+            </Box>
+          </Box>
         </Box>
+        {/* standard package */}
+        <Box width="100%" mt={3}>
+          <Box mb={2}>
+            <Typography align="center" variant="h6">
+              Package - Standard
+            </Typography>
+          </Box>
+
+          <TextField
+            {...register("basicPrice")}
+            label="Price"
+            helpertext={errors.basicPrice?.message}
+            error={errors.basicPrice ? true : false}
+            variant="outlined"
+            type="number"
+            className={classes.formInput}
+          />
+          <TextField
+            {...register("basicDeliveryTime")}
+            label="Delivery Time (days)"
+            helpertext={errors.basicDeliveryTime?.message}
+            error={errors.basicDeliveryTime ? true : false}
+            variant="outlined"
+            type="number"
+            className={classes.formInput}
+          />
+          {/* features */}
+          <Box width="100%">
+            <Box display="flex" justifyContent="center" alignItems="center">
+              <TextField
+                label="Features"
+                helpertext="5 revision, 3 pages, source code, etc"
+                variant="outlined"
+                value={features.standard}
+                onChange={(e) =>
+                  setFeatures((prev) => {
+                    return { ...prev, standard: e.target.value };
+                  })
+                }
+                className={classes.formInput}
+              />
+              <Box ml={1} mt={-7}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={(e) => {
+                    if (features.standard === "") return;
+                    setStandardFeatures((prev) =>
+                      prev ? [...prev, features.standard] : [features.standard]
+                    );
+
+                    setFeatures((prev) => {
+                      return { ...prev, standard: "" };
+                    });
+                  }}
+                >
+                  Add
+                </Button>
+              </Box>
+            </Box>
+            <Box display="flex" flexWrap="wrap">
+              {!standardFeatures || standardFeatures.length <= 0 ? (
+                <Typography>Not yet added.</Typography>
+              ) : (
+                standardFeatures.map((feature, idx) => (
+                  <Box
+                    className={classes.hover}
+                    onClick={() =>
+                      setStandardFeatures((prev) =>
+                        prev.filter((item) => item !== feature)
+                      )
+                    }
+                  >
+                    <RoundedBox
+                      key={idx}
+                      light={true}
+                      icon={true}
+                      borderColor={
+                        idx % 2 == 0 ? "primary.main" : "secondary.main"
+                      }
+                    >
+                      {feature}
+                      <AiFillDelete />
+                    </RoundedBox>
+                  </Box>
+                ))
+              )}
+            </Box>
+          </Box>
+        </Box>
+        {/* premium package */}
+        <Box width="100%" mt={3}>
+          <Box mb={2}>
+            <Typography align="center" variant="h6">
+              Package - Premium
+            </Typography>
+          </Box>
+
+          <TextField
+            {...register("premiumPrice")}
+            label="Price"
+            helpertext={errors.premiumPrice?.message}
+            error={errors.premiumPrice ? true : false}
+            variant="outlined"
+            type="number"
+            className={classes.formInput}
+          />
+          <TextField
+            {...register("premiumDeliveryTime")}
+            label="Delivery Time (days)"
+            helpertext={errors.premiumDeliveryTime?.message}
+            error={errors.premiumDeliveryTime ? true : false}
+            variant="outlined"
+            type="number"
+            className={classes.formInput}
+          />
+          {/* features */}
+          <Box width="100%">
+            <Box display="flex" justifyContent="center" alignItems="center">
+              <TextField
+                label="Features"
+                helpertext="5 revision, 3 pages, source code, etc"
+                variant="outlined"
+                value={features.premium}
+                onChange={(e) =>
+                  setFeatures((prev) => {
+                    return { ...prev, premium: e.target.value };
+                  })
+                }
+                className={classes.formInput}
+              />
+              <Box ml={1} mt={-7}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={(e) => {
+                    if (features.premium === "") return;
+                    setPremiumFeatures((prev) =>
+                      prev ? [...prev, features.premium] : [features.premium]
+                    );
+
+                    setFeatures((prev) => {
+                      return { ...prev, premium: "" };
+                    });
+                  }}
+                >
+                  Add
+                </Button>
+              </Box>
+            </Box>
+            <Box display="flex" flexWrap="wrap">
+              {!premiumFeatures || premiumFeatures.length <= 0 ? (
+                <Typography>Not yet added.</Typography>
+              ) : (
+                premiumFeatures.map((feature, idx) => (
+                  <Box
+                    className={classes.hover}
+                    onClick={() =>
+                      setPremiumFeatures((prev) =>
+                        prev.filter((item) => item !== feature)
+                      )
+                    }
+                  >
+                    <RoundedBox
+                      key={idx}
+                      light={true}
+                      icon={true}
+                      borderColor={
+                        idx % 2 == 0 ? "primary.main" : "secondary.main"
+                      }
+                    >
+                      {feature}
+                      <AiFillDelete />
+                    </RoundedBox>
+                  </Box>
+                ))
+              )}
+            </Box>
+          </Box>
+        </Box>
+
+        <Button type="submit" variant="outlined" color="primary" size="large">
+          Submit
+        </Button>
       </form>
     </Container>
   );
