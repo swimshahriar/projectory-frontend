24caf481545ab95503c9c4f5193b1ad6d7b3834d
Shahriar Swim <shahriar2k18@gmail.com>
2021-08-21 10:37:48 +0600

    skillTest card

---
+++ src/components/SkillTestCard.jsx
@@ -0,0 +1,43 @@
+import { Box, Button, Chip, Paper, Typography } from "@material-ui/core";
+import React from "react";
+
+const SkillTestCard = ({ skillTest }) => {
+  const func = {};
+  return (
+    <Box minWidth="18rem" width="30%" maxWidth="22rem" flexGrow={1}>
+      <Paper>
+        <Box py={3} px={2}>
+          <Typography variant="h6" gutterBottom>
+            {skillTest.title}
+          </Typography>
+          <Typography variant="body1" color="textSecondary">
+            {skillTest.about}
+          </Typography>
+          <Box
+            display="flex"
+            justifyContent="flex-start"
+            alignItems="center"
+            flexWrap="wrap"
+            gridGap={12}
+            my={3}
+          >
+            <Chip color="secondary" label={`${Object.keys(skillTest.questions).length} ques`} />
+            <Chip color="primary" label={`${skillTest.duration} mins`} />
+            <Chip
+              color="secondary"
+              variant="outlined"
+              label={`${skillTest.timesTaken} times taken`}
+            />
+          </Box>
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <Button variant="contained" color="primary">
+              Start
+            </Button>
+          </Box>
+        </Box>
+      </Paper>
+    </Box>
+  );
+};
+
+export default SkillTestCard;
--- src/pages/skillTests/SkillTestList.jsx
+++ src/pages/skillTests/SkillTestList.jsx
@@ -3,10 +3,13 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 // internal imports
 import { fetchSkillTestResults, fetchSkillTests } from "../../actions/skillTestAction";
+import Loading from "../../components/Loading";
+import SkillTestCard from "../../components/SkillTestCard";
 
 const SkillTestList = () => {
   const dispatch = useDispatch();
   const { uid, token } = useSelector((state) => state.auth);
+  const { skillTests, testResults, isLoading } = useSelector((state) => state.skillTest);
 
   // fetch available skill tests
   useEffect(() => {
@@ -23,17 +26,27 @@ const SkillTestList = () => {
 
   return (
     <Container maxWidth="lg">
+      {/* ----------------------- skill tests list -------------------- */}
       <Box my={3}>
         <Typography variant="h4" align="center">
           Skill Tests
         </Typography>
       </Box>
+      <Box display="flex" justifyContent="center" alignItems="center" flexWrap="wrap" gridGap={15}>
+        {isLoading && !skillTests ? (
+          <Loading />
+        ) : (
+          skillTests?.map((skillTest, idx) => <SkillTestCard skillTest={skillTest} key={idx} />)
+        )}
+      </Box>
 
+      {/* ---------------------- test results ------------------------- */}
       <Box my={3}>
         <Typography variant="h4" align="center">
           Your Results
         </Typography>
       </Box>
+      <Box>{isLoading && !testResults && <Loading />}</Box>
     </Container>
   );
 };
