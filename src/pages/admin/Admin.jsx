4da12c2a11d9ca8604c3eeae4a56e410b6b2a16b
Shahriar Swim <shahriar2k18@gmail.com>
2021-08-28 23:24:08 +0600

    admin dashboard design demo

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, Typography } from "@material-ui/core";
+import { Box, Container, Paper, Typography } from "@material-ui/core";
 import React from "react";
 // internal imports
 import AdminLayout from "../../components/layouts/AdminLayout";
@@ -12,6 +12,59 @@ const Admin = () => {
           <Typography variant="h5" align="center">
             Dashboard
           </Typography>
+        </Box>
+
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="flex-start"
+          flexWrap="wrap"
+          gridGap={15}
+        >
+          {/* --------------------- balance ------------------ */}
+          <Paper>
+            <Box minWidth="12rem" p={2}>
+              <Box>
+                <Typography variant="h6" color="textSecondary">
+                  Balance
+                </Typography>
+              </Box>
+              <Typography variant="body1" color="textPrimary">
+                Current: 35000tk
+              </Typography>
+            </Box>
+          </Paper>
+
+          {/* ---------------------- active jobs/services -------------------- */}
+          <Paper>
+            <Box minWidth="12rem" p={2}>
+              <Box>
+                <Typography variant="h6" color="textSecondary">
+                  Active
+                </Typography>
+              </Box>
+              <Typography variant="body1" color="textPrimary">
+                jobs: 15
+              </Typography>
+              <Typography variant="body1" color="textPrimary">
+                services: 22
+              </Typography>
+            </Box>
+          </Paper>
+
+          {/* ----------------------- total users ----------------------- */}
+          <Paper>
+            <Box minWidth="12rem" p={2}>
+              <Box>
+                <Typography variant="h6" color="textSecondary">
+                  Balance
+                </Typography>
+              </Box>
+              <Typography variant="body1" color="textPrimary">
+                Current: 35000tk
+              </Typography>
+            </Box>
+          </Paper>
         </Box>
       </Container>
     </AdminLayout>
